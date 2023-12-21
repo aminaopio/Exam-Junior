85560b5088eaeb89a7449e43383f28674e79a2ce
Amina <aminaopio@my.unt.edu>
2023-12-20 18:24:11 -0600

    Rearranging files

--- obj/Debug/net8.0/InterviewTest.AssemblyInfo.cs
+++ obj/Debug/net8.0/InterviewTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InterviewTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34cff74d7e24fb092c478afd41f5348c813ea2c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef763ea66475d3f6caec918667f3bb62a30991ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("InterviewTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InterviewTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
